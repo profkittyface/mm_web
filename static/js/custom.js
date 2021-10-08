7c0b969e11704418f84e4e8c81c5ae30f6d12764
Andrew Hunt <ahunt.tesselation@gmail.com>
2021-10-07 17:10:31 -0700

    Adding dadjoke

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,1 +1,11 @@
 console.log("custom.js")
+
+$.ajax({
+    type: "GET",
+    dataType: "json",
+    url: "https://icanhazdadjoke.com/",
+    complete: function(data) {
+        console.log(data.responseJSON.joke)
+        $('.dadjoke').html(data.responseJSON.joke);
+    }
+});
